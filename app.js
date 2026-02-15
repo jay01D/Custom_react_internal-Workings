d598b75a4e0b095ffc6837acc8375794c3db351a
jay01D <jaydoshi0109@gmail.com>
2026-02-15 14:47:27 +0530

    shows how createElement works in react

--- app.js
+++ app.js
@@ -3,18 +3,41 @@ const root = document.getElementById("root");
 // const element = document.createElement("h1");
 // element.textContent = "Hello World";
 
-function createElement(tag, attribute, children) {
-  const element = document.createElement(tag);
-  element.textContent = children;
-  for (const key in attribute) {
-    element[key] = attribute[key];
-  }
-  return element;
-}
+// function createElement(tag, attribute, children) {
+//   const element = document.createElement(tag);
+//   element.textContent = children;
+//   for (const key in attribute) {
+//     if (key === "style") {
+//       Object.assign(element.style, attribute.style);
+//     } else {
+//       element[key] = attribute[key];
+//     }
+//   }
+//   return element;
+// }
 
-const element = createElement(
+const React = {
+  createElement: function (tag, attribute, children) {
+    const element = document.createElement(tag);
+    element.textContent = children;
+    for (const key in attribute) {
+      if (key === "style") {
+        Object.assign(element.style, attribute.style);
+      } else {
+        element[key] = attribute[key];
+      }
+    }
+    return element;
+  },
+};
+
+const element = React.createElement(
   "h1",
-  { className: "heading", id: "head" },
+  {
+    className: "heading",
+    id: "head",
+    style: { fontSize: "20px", backgroundColor: "orange" },
+  },
   "hello from h1",
 );
 
